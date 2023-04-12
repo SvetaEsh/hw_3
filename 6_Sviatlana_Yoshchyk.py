436128362bb4ed7adc31e4c2f450eafe945701de
Sviatlana E <Svetic010289@gmail.com>
2023-04-12 12:40:48 +0300

    add hw_6

---
+++ 6_Sviatlana_Yoshchyk.py
@@ -0,0 +1,20 @@
+data_cal_bme = {"1":
+                {"Petya":
+                 {"height": 1.62,
+                  "weight": 75,
+                  "gender": "male",
+                  "age": 30}}, 
+                "2": 
+                {"Vasya":
+                 {"height": 1.82,
+                  "weight": 75,
+                  "gender": "male",
+                  "age": 28}},
+                "3":
+                {"Sveta":
+                 {"height": 1.64,
+                  "weight": 69,
+                  "gender": "female",
+                  "age": 34}}
+                  }
+print(data_cal_bme.items())
