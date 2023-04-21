7a6cad456c3b17448c55328660a6afe6fcaf9e7b
Sviatlana E <Svetic010289@gmail.com>
2023-04-21 16:28:38 +0300

    add :

--- 8_Clock.py
+++ 8_Clock.py
@@ -1,41 +1,16 @@
 import datetime
 import os
 import time
-
-"""
-
-current_date_time = datetime.datetime.now()
-current_time = current_date_time.time()
-out_time=current_time.strftime("%H%M%S")
-with open("numbers", "r", encoding="utf-8") as file_numbers:
-        text = file_numbers.readlines()
-        
-        text_new=[]
-        for i in text:
-            i_new=i.replace("\n", "")
-            
-            text_new.append(i_new)
-
-        print(text)
-        print()
-        print(text_new)
-        
-
-        for i in range(5):
-            a=""
-            for el in out_time:
-                n=int(el)
-                a=a+text_new[n*5:(n+1)*5][i]+"     "
-            print(a)
-"""
-
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 
 while True:
     current_date_time = datetime.datetime.now()
     current_time = current_date_time.time()
-    out_time=current_time.strftime("%H%M%S")
-    
+    out_time=current_time.strftime("%H:%M:%S")
+    out_time_sec=current_time.strftime("%S")
     
     
     
@@ -50,8 +25,15 @@ while True:
         for i in range(5):
             a=""
             for el in out_time:
-                n=int(el)
-                a=a+text_new[n*5:(n+1)*5][i]+"     "
+                if el in "0123456789":
+                    n=int(el)
+                    a=a+text_new[n*5:(n+1)*5][i]+"     "
+                else:
+                    if int(out_time_sec)%2==0:
+                        a=a+text_new[50:55][i]+"     "
+                    else:
+                        a=a+"     "+"     "
+                    
             print(a)
         
         
